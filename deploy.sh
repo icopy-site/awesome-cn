210ffe66a291e8b7b4560579976ff6487d3f6152
chenjiajia <jiajia.chen@dewmobile.net>
2019-02-22 19:05:16 +0800

    索引只构建标题和目录

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,6 @@ if [ "$1" != "dry" ]; then
     git config user.name "Travis Builder"
     git config user.email "$EMAIL"
 fi
+cp search_index.py /home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/mkdocs/contrib/search/search_index.py
 
 mkdocs gh-deploy -v --clean --force --remote-name gh-token;
